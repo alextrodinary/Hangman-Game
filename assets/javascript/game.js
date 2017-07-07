2775e2dec11a1057609f0c0ff929e8d143691f8e
Alex Nguyen <a_nguyen01@yahoo.com>
2017-07-06 19:49:14 -0700

    game now generates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,7 +13,6 @@ function startGame(){
     var nbaTeams = ["lakers", "cavaliers", "warriors", "spurs"];
 	var randomTeam = Math.floor(Math.random() * nbaTeams.length);
 	gameChosen = (nbaTeams[randomTeam]);
-	//console.log("random team selected");
 	if(gameChosen === "lakers"){
 		//console.log("lakers game");
 		for(var i = 0; i < 6; i++){
@@ -24,8 +23,9 @@ function startGame(){
 			spaces.setAttribute("id", letters[i]); 
 		}
 	}
-	if(gameChosen === "cavaliers")
-		//console.log("cavaliers game");
+
+
+	if(gameChosen === "cavaliers"){
 		for(var i = 0 ; i < 9; i++){
 			var letters = ['1', '2', '3', '4', '5', '6', '7', '8'];
 			var spaces = document.createElement("letterSpace");
@@ -35,26 +35,27 @@ function startGame(){
 
 		}
 	}
-	if(gameChosen === "warriors") {
-		//console.log("warriors game");
+
+	if(gameChosen === "warriors"){
 			for(var i =0; i < 8 ; i++){
 				var letters = ['1', '2', '3', '4', '5', '6', '7', '8'];
 				var spaces = document.createElement("letterSpace");
 				spaces.innerHTML = "__ ";
 				gamesDiv.appendChild(spaces);
 				spaces.setAttribute("id", letters[i]);
-			}
+		}
 	}
+
 	if(gameChosen === "spurs"){
-		//console.log ("spurs game");
 			for(var i =0; i < 5; i++){
 				var letters = ['1', '2', '3', '4','5'];
 				var spaces = document.createElement("letterSpace");
 				spaces.innerHTML = "__ ";
 				gamesDiv.appendChild(spaces);
 				spaces.setAttribute("id", letters[i]);
-			}
+		}
 	}
+
 	document.getElementById("status").innerHTML = "Guesses Left: 6";
 }
 
@@ -68,19 +69,19 @@ document.onkeypress = function(event){
 		if(guesses === "l"){
 			document.getElementById("1").innerHTML = "l";
 		}
-		if(guesses === "2"){
+		if(guesses === "a"){
 			document.getElementById("2").innerHTML = "a";
 		}
-		if(guesses === "3"){
+		if(guesses === "k"){
 			document.getElementById("3").innerHTML = "k";
 		}
-		if(guesses === "4"){
+		if(guesses === "e"){
 			document.getElementById("4").innerHTML = "e";
 		}
-		if(guesses === "5"){
+		if(guesses === "r"){
 			document.getElementById("5").innerHTML = "r";
 		}
-		if(guesses === "6"){
+		if(guesses === "s"){
 			document.getElementById("6").innerHTML = "s";
 		}
 
@@ -129,31 +130,31 @@ document.onkeypress = function(event){
 	if(gameChosen === "cavaliers") {
 		console.log("cavaliers game");
 		var wrongLetter = ["b", "d", "f", "g", "h","j", "k","m", "n", "o", "q", "t", "u", "w", "x", "y", "z"];
-		if(guesses === "l"){
+		if(guesses === "c"){
 			document.getElementById("1").innerHTML = "c";
 		}
-		if(guesses === "2"){
+		if(guesses === "a"){
 			document.getElementById("2").innerHTML = "a";
 		}
-		if(guesses === "3"){
+		if(guesses === "v"){
 			document.getElementById("3").innerHTML = "v";
 		}
-		if(guesses === "4"){
+		if(guesses === "a"){
 			document.getElementById("4").innerHTML = "a";
 		}
-		if(guesses === "5"){
+		if(guesses === "l"){
 			document.getElementById("5").innerHTML = "l";
 		}
-		if(guesses === "6"){
+		if(guesses === "i"){
 			document.getElementById("6").innerHTML = "i";
 		}
-		if(guesses === "7"){
+		if(guesses === "e"){
 			document.getElementById("7").innerHTML = "e";
 		}
-		if(guesses === "8"){
+		if(guesses === "r"){
 			document.getElementById("8").innerHTML = "r";
 		}
-		if(guesses === "9"){
+		if(guesses === "s"){
 			document.getElementById("9").innerHTML = "s";
 		}
 		if((document.getElementById('1').innerHTML === "l") &&
@@ -204,28 +205,28 @@ document.onkeypress = function(event){
 	if(gameChosen === "warriors") {
 		console.log("warriors game");
 		var wrongLetter = ["b", "c", "d", "e", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "t", "u", "x", "y", "z"];
-		if(guesses === "l"){
+		if(guesses === "w"){
 			document.getElementById("1").innerHTML = "w";
 		}
-		if(guesses === "2"){
+		if(guesses === "a"){
 			document.getElementById("2").innerHTML = "a";
 		}
-		if(guesses === "3"){
+		if(guesses === "r"){
 			document.getElementById("3").innerHTML = "r";
 		}
-		if(guesses === "4"){
+		if(guesses === "r"){
 			document.getElementById("4").innerHTML = "r";
 		}
-		if(guesses === "5"){
+		if(guesses === "i"){
 			document.getElementById("5").innerHTML = "i";
 		}
-		if(guesses === "6"){
+		if(guesses === "o"){
 			document.getElementById("6").innerHTML = "o";
 		}
-		if(guesses === "7"){
+		if(guesses === "r"){
 			document.getElementById("7").innerHTML = "r";
 		}
-		if(guesses === "8"){
+		if(guesses === "s"){
 			document.getElementById("8").innerHTML = "s";
 		}
 		if((document.getElementById('1').innerHTML === "l") &&
@@ -276,19 +277,19 @@ document.onkeypress = function(event){
 	if(gameChosen === "spurs") {
 		console.log("spurs game");
 		var wrongLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "q", "t", "u", "w", "x", "y", "z"];
-		if(guesses === "l"){
+		if(guesses === "s"){
 			document.getElementById("1").innerHTML = "s";
 		}
-		if(guesses === "2"){
+		if(guesses === "p"){
 			document.getElementById("2").innerHTML = "p";
 		}
-		if(guesses === "3"){
+		if(guesses === "u"){
 			document.getElementById("3").innerHTML = "u";
 		}
-		if(guesses === "4"){
+		if(guesses === "r"){
 			document.getElementById("4").innerHTML = "r";
 		}
-		if(guesses === "5"){
+		if(guesses === "s"){
 			document.getElementById("5").innerHTML = "s";
 		}
 		if((document.getElementById('1').innerHTML === "l") &&
